540247c2ccd8b2cf6a2edfa7ede9da8d4d8b5e84
jdsuarez <jdsuarez@fgs.co>
2024-12-15 13:14:28 -0500

    add Stripe integration to payments service; update environment variables and package dependencies

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,11 +4,13 @@ import * as joi from 'joi';
 
 interface IEnvs {
   PORT: number;
+  STRIPE_SECRET: string;
 }
 
 const envsSchema = joi
   .object<IEnvs>({
     PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -22,4 +24,5 @@ const envVars: IEnvs = value;
 
 export const envs = {
   PORT: envVars.PORT,
+  STRIPE_SECRET: envVars.STRIPE_SECRET,
 };
