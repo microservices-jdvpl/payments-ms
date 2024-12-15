ec3f939ba5bc03e9fd99ae5e3db91472ce122586
jdsuarez <jdsuarez@fgs.co>
2024-12-15 12:46:01 -0500

    add payments module with controller and service; configure environment variables and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/env';
 
 async function bootstrap() {
+  const logger = new Logger('Payments-ms');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(envs.PORT);
+  logger.log(`Payments-ms is running on http://localhost:${envs.PORT}`);
 }
 bootstrap();
---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+  @Post('create-payment-session')
+  createrPaymentSession() {
+    return { h: 'hello' };
+  }
+  @Get('success')
+  success() {
+    return {
+      ok: true,
+      message: 'Payment was successful',
+    };
+  }
+
+  @Get('cancel')
+  cancel() {
+    return {
+      ok: true,
+      message: 'Payment was canceled',
+    };
+  }
+
+  @Post('webhook')
+  async stripeWebhook() {
+    return {
+      ok: true,
+      message: 'Webhook received',
+    };
+  }
+}
