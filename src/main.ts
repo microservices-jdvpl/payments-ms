bd09ef8caaa1f78432f76035a8d0b0fe320152de
jdsuarez18 <jdsuarez@fgs.co>
2025-01-08 13:18:47 -0500

    add validation and DTO for payment session; update PaymentsService and PaymentsController to handle payment session data

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/env';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   app.setGlobalPrefix('api');
   await app.listen(envs.PORT);
   logger.log(`Payments-ms is running on http://localhost:${envs.PORT}`);
